2261fe763b98edef1880978b6fea8b52ee8e0266
= <jan-janosch.schneider@gmx.de>
2021-10-27 14:47:34 +0200

    edited setup file

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ with open('README.rst') as f:
 
 setup(
     name='taxodist',
-    version='v0.0.0',
+    version='0.0.0',
     url='https://github.com/janschneida/taxodist',
     license='MIT',
     author='Jan Janosch Schneider',
     author_email='janjanosch.schneider@med.uni-goettingen.de',
     description='Package for distance calculations of concepts in taxonomic hierarchies.',
-    #long_description=README,
+    long_description=README,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     packages=find_packages(),
     python_requires=">=3.9.6",
-    install_requires=['numpy>=1.21.1', 'scikit-learn>=0.24.2', 'pandas>=1.3.0', 'treelib==1.6.1']
+    install_requires=['scipy>=1.3.1','numpy>=1.16.5', 'scikit-learn>=0.21.3', 'pandas>=0.25.1', 'treelib==1.6.1']
 )
