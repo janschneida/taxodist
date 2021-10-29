331a6f429666f58155a72cc1215139063ffb22e7
= <jan-janosch.schneider@gmx.de>
2021-10-29 17:33:11 +0200

    moved tests from Class to own file, added docs

--- taxodist/taxodist.py
+++ taxodist/taxodist.py
@@ -1,86 +1,84 @@
-
-import pandas as pd
-import icd10Utils as utils
+import taxodist_utils as utils
 import numpy as np
 import concurrent.futures as cf
 from timeit import default_timer as timer
 
+class DistanceCalculations:
+    def calc_distance_with_codes(max_workers=None,codes=None,parallelized=True,plot_when_finished=False):
+        '''
+        Visualizes the similarity of ICD10 codes based on the ICD10 taxonomy. \n
+        Saves x and y coordiantes of the codes in an excel-sheet for further distance calculation. \n
+        Saves pairwise distances of the codes in another excel-sheet.\n
 
-def main(max_workers=None, code_cnt=None):
-    '''Visualizes the similarity of ICD10 codes based on the ICD10 taxonomy. \n
-    Saves x and y coordiantes of the codes in excel-table for further distance calculation.'''
+            Parameters:
+                max_workers (int): 
+                                The number of parallel processes to calculate the distances. \n
+                                This number must not be greater then the cores your system offers. \n
+                                Per default, concurrent.futures picks the "best" setting for your system.
+                codes (list): 
+                                A list of ICD-10 codes to calculate the distances based on their position in the ICD-10-GM hierarchy. \n
+                                Per default, this method calculates a complete list of all available ICD-10-GM codes.
+                parallelized (bool):
+                                Sets whether or not the calculation should be parallelized. \n 
+                                Especially for smaller code-batch-sizes it might make sense to use the serialized calculation, 
+                                because parallelization overhead might outweigh its gain. \n 
+                                Per default, the method runs in parallel.
+                plot_when_finished (bool):
+                                Sets whether or not to plot the relative distances of the codes after the calculation.
 
-    ######################### SETUP #########################
+            Returns:
+                max_workers (int):
+                                The degree of parallelization.
+                code_cnt (int):
+                                The number of codes for which their distances have been calculated.
+                runtime [s] (float):
+                                The time in seconds it took to calculate the distances.
+        '''
 
-    tree = utils.buildICD10Tree()
-    code_cnt = 1000
-    # computation with all ICD codes
-    ICD10_codes = utils.getAllICD10Codes(tree)
-    if code_cnt:
-        ICD10_codes = ICD10_codes[:code_cnt]
+        ######################### SETUP #########################
 
-    # computation with 36 example codes
-    # ICD10_codes = utils.getICD10CodesFromExcel()
+        tree = utils.buildICD10Tree()
+        ICD10_codes = codes
+        if not ICD10_codes:
+            ICD10_codes = utils.getAllICD10Codes(tree)
 
-    length = len(ICD10_codes)
-    dist_matrix = np.zeros(shape=(len(ICD10_codes), len(ICD10_codes)))
+        length = len(ICD10_codes)
+        dist_matrix = np.zeros(shape=(len(ICD10_codes), len(ICD10_codes)))
 
-    if not max_workers:
-    ################## SEQUENTIAL COMPUTATION ##################
-        start = timer() 
-        utils.getDistMatrixSeq(ICD10_codes,tree,dist_matrix)
+        if not parallelized:
+        ################## SEQUENTIAL COMPUTATION ##################
+            start = timer() 
+            utils.getDistMatrixSeq(ICD10_codes,tree,dist_matrix)
+            time = timer() - start
+            return 0,length,time
+
+        ################## PARALLELIZED COMPUTATION ##################
+
+        fs = []
+        with cf.ProcessPoolExecutor(max_workers=max_workers) as executor:
+            max_workers = executor.__getattribute__('_max_workers')
+            start = timer()
+            for i in range(0,max_workers):
+                # start processes and save return values 
+                fs.append(executor.submit(utils.getDistMatrixWrapper, (ICD10_codes, tree, i+1, max_workers)))
+        for future in cf.as_completed(fs):
+            # merge partial matrices
+            partial_dist_matrix, worker_index = future.result()
+            dist_matrix[utils.getStart(worker_index,max_workers,length):utils.getStop(worker_index,max_workers,length)] = partial_dist_matrix
+        
+        dist_matrix = utils.mirrorMatrix(dist_matrix)
         time = timer() - start
-        return code_cnt,time
-
-    ################## PARALLELIZED COMPUTATION ##################
-
-    fs = []
-    with cf.ProcessPoolExecutor(max_workers=max_workers) as executor:
-        max_workers = executor.__getattribute__('_max_workers')
-        # print('max workers: ',max_workers)
-        start = timer()
-        for i in range(0,max_workers):
-            # start processes and save return values 
-            fs.append(executor.submit(utils.getDistMatrixWrapper, (ICD10_codes, tree, i+1, max_workers)))
-    for future in cf.as_completed(fs):
-        # merge partial matrices
-        partial_dist_matrix, worker_index = future.result()
-        dist_matrix[utils.getStart(worker_index,max_workers,length):utils.getStop(worker_index,max_workers,length)] = partial_dist_matrix
     
-    dist_matrix = utils.mirrorMatrix(dist_matrix)
-    time = timer() - start
- 
-    print('Calculation time: ', time, ' seconds')
-    
-    dist_matrix = utils.mirrorMatrix(dist_matrix)
+        # print('Calculation time: ', time, ' seconds')
+        
+        dist_matrix = utils.mirrorMatrix(dist_matrix)
 
-    df_mds_coordinates = utils.getMDSMatrix(dist_matrix)
+        df_mds_coordinates = utils.getMDSMatrix(dist_matrix)
 
-    # utils.saveCodeDistancesInExcel(df_mds_coordinates, ICD10_codes)
+        utils.saveCodeDistancesInExcel(df_mds_coordinates, ICD10_codes)
 
-    utils.plotCodes(df_mds_coordinates, ICD10_codes)
-    return max_workers, time
+        if plot_when_finished:
+            utils.plotCodes(df_mds_coordinates, ICD10_codes)
+        return max_workers,length,time
 
-if __name__ == "__main__":
-    main(max_workers=8)
-    # runtimes = []
-    # for i in range(1,9):
-    #     runtimes.append(main(i,100))
-    # df_runtimes = pd.DataFrame(runtimes)
-    # df_runtimes.to_excel('parallel_runtimes_100_codes.xlsx')
-
-    # for i in range(1,9):
-    #     runtimes.append(main(i,2000))
-    # df_runtimes = pd.DataFrame(runtimes)
-    # df_runtimes.to_excel('parallel_runtimes_2000_codes.xlsx')
-
-    # for i in range(1,9):
-    #     runtimes.append(main(i))
-    # df_runtimes = pd.DataFrame(runtimes)
-    # df_runtimes.to_excel('parallel_runtimes_all_codes.xlsx')
-
-    # for code_cnt in [100, 2000, None]:
-    #      runtimes.append(main(code_cnt=code_cnt))
-    # df_runtimes = pd.DataFrame(runtimes)
-    # df_runtimes.to_excel('seq_runtimes.xlsx')
-    
+        
