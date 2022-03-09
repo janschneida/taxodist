62fe61543392ae8b9a1b0c9e7a871ffafda6cf89
Jan Schneider <jan-janosch.schneider@gmx.de>
2022-03-09 16:07:10 +0100

    wrote IC tests

--- tests/dist_calc_validation.py
+++ tests/dist_calc_validation.py
@@ -15,11 +15,70 @@ def main():
     tree = getTestTree()
     depth = tree.depth
 
-    cs = utils.getCS('','',tree,depth)
-    assert cs == 0.5
+    testICs(tree)
+    testCSs(tree,depth)
 
-    cs = utils.getCS('','',tree,depth)
-    assert cs == 0.5
+def testCSs(tree, depth):
+
+    # WU PALMER TESTS
+
+    cs = utils.getCS(1,1,tree,depth,'level','wu_palmer')
+    assert math.isclose(cs,2.639,rel_tol=0.01)
+
+    cs = utils.getCS(1,9,tree,depth,'level','wu_palmer')
+    assert math.isclose(cs,2.639,rel_tol=0.01)
+
+    cs = utils.getCS(13,31,tree,depth,'level','wu_palmer')
+    assert math.isclose(cs,2.639,rel_tol=0.01)
+
+    cs = utils.getCS(30,31,tree,depth,'level','wu_palmer')
+    assert math.isclose(cs,2.639,rel_tol=0.01)
+
+    # LI TESTS
+
+    cs = utils.getCS(1,1,tree,depth,'level','li')
+    assert math.isclose(cs,2.639,rel_tol=0.01)
+
+    cs = utils.getCS(1,9,tree,depth,'level','li')
+    assert math.isclose(cs,2.639,rel_tol=0.01)
+
+    cs = utils.getCS(13,31,tree,depth,'level','li')
+    assert math.isclose(cs,2.639,rel_tol=0.01)
+
+    cs = utils.getCS(30,31,tree,depth,'level','li')
+    assert math.isclose(cs,2.639,rel_tol=0.01)
+
+
+def testICs(tree):
+    ic = utils.getIC(0,tree,'level')
+    assert ic == 0
+
+    ic = utils.getIC(1,tree,'level')
+    assert ic == 1
+
+    ic = utils.getIC(10,tree,'level')
+    assert ic == 2
+
+    ic = utils.getIC(20,tree,'level')
+    assert ic == 3
+
+    ic = utils.getIC(31,tree,'level')
+    assert ic == 4
+
+    ic = utils.getIC(0,tree,'sanchez')
+    assert ic == 0.0
+
+    ic = utils.getIC(1,tree,'sanchez')
+    assert math.isclose(ic,1.386,rel_tol=0.01)
+
+    ic = utils.getIC(10,tree,'sanchez')
+    assert math.isclose(ic,2.128,rel_tol=0.01)
+
+    ic = utils.getIC(20,tree,'sanchez')
+    assert math.isclose(ic,2.233,rel_tol=0.01)
+
+    ic = utils.getIC(30,tree,'sanchez')
+    assert math.isclose(ic,2.639,rel_tol=0.01)
 
 def getTestTree():
     tree = treelib.Tree()
