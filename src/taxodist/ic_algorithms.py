b5467d6dd7e46570bda19f9a083d51656c8b5575
Jan Schneider <jan-janosch.schneider@gmx.de>
2022-04-22 11:51:14 +0200

    exported ic algs to own file

---
+++ src/taxodist/ic_algorithms.py
@@ -0,0 +1,23 @@
+from treelib import Tree
+import td_utils as utils
+import math
+
+def getICSanchez(concept: str, tree: Tree):
+    """IC calculation based on Sánchez et al. https://doi.org/10.1016/j.knosys.2010.10.001"""
+    if concept == tree.root:
+        return 0.0
+    
+    # we have to add 1 to this set, because Sánchez et al. defined the set of subsumer so, 
+    # that the concept itself is part of the set
+    subsumers = len(utils.getAncestors(concept,tree)) + 1 
+    # subsumer(root) = 1 according to Sánchez at el.
+    if subsumers == 1:
+        return 0.0
+
+    if tree.get_node(concept).is_leaf():
+        subtree_leaves_cnt = 0
+    else:
+        subtree_leaves_cnt = len(tree.leaves(concept))
+
+    all_leaves_cnt = len(tree.leaves()) 
+    return -math.log( ( (subtree_leaves_cnt/subsumers) + 1)/(all_leaves_cnt+1) )
--- src/taxodist/td_utils.py
+++ src/taxodist/td_utils.py
@@ -13,6 +13,7 @@ from sklearn.manifold import MDS
 from treelib.node import Node
 from treelib.tree import Tree
 import cs_algorithms
+import ic_algorithms
 
 max_ic = None
 
@@ -34,33 +35,12 @@ def getIC(concept: str, tree: Tree, ic_mode: str):
             # IC calculation based on Boriah et al. https://doi.org/10.1137/1.9781611972788.22 
             return tree.depth(concept)
         elif ic_mode == 'sanchez':
-            return getICSanchez(concept,tree)
+            return ic_algorithms.getICSanchez(concept,tree)
         else:
             raise ValueError('Unsupported IC-mode: ',ic_mode)
     except ValueError as err:
         print(err.args)
         sys.exit()
-
-def getICSanchez(concept: str, tree: Tree):
-    """IC calculation based on Sánchez et al. https://doi.org/10.1016/j.knosys.2010.10.001"""
-    if concept == tree.root:
-        return 0.0
-    
-    # we have to add 1 to this set, because Sánchez et al. defined the set of subsumer so, 
-    # that the concept itself is part of the set
-    subsumers = len(getAncestors(concept,tree)) + 1 
-    # subsumer(root) = 1 according to Sánchez at el.
-    if subsumers == 1:
-        return 0.0
-
-    if tree.get_node(concept).is_leaf():
-        subtree_leaves_cnt = 0
-    else:
-        subtree_leaves_cnt = len(tree.leaves(concept))
-
-    all_leaves_cnt = len(tree.leaves()) 
-    return -math.log( ( (subtree_leaves_cnt/subsumers) + 1)/(all_leaves_cnt+1) )
-
 
 def getLCA(concept1: str, concept2: str, tree: Tree, ic_mode: str) -> str:
     """Return lowest common ancester of two concepts."""
