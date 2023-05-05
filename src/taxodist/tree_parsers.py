2351f6168ba72ffedb81baef073185f67b61531c
= <jan-janosch.schneider@gmx.de>
2023-05-05 10:56:30 +0200

    fixed imports and resource call

--- src/taxodist/tree_parsers.py
+++ src/taxodist/tree_parsers.py
@@ -1,7 +1,10 @@
 from re import A
 import treelib
 import xml.etree.ElementTree as ET
-from src.taxodist import td_utils as utils
+import sys
+import os
+sys.path.append(os.getcwd())
+import td_utils as utils
 
 def getICD10GMTree(version = '2022'):
     """
@@ -9,9 +12,9 @@ def getICD10GMTree(version = '2022'):
     Based on the ICD-10-XML export from https://www.dimdi.de/dynamic/de/klassifikationen/downloads/
     """
     if(version == '2021'):
-        raw_xml = ET.parse('resources\\ICD_10_GM_2021.xml')
+        raw_xml = ET.parse('resources/ICD_10_GM_2021.xml')
     elif(version == '2022'):
-        raw_xml = ET.parse('resources\\ICD_10_GM_2022.xml')
+        raw_xml = ET.parse('resources/ICD_10_GM_2022.xml')
     root = raw_xml.getroot()
     tree = treelib.Tree()
     tree.create_node('ICD-10', 0)
@@ -119,7 +122,7 @@ def getICD10CMTree():
     Returns a tree that represents the ICD-10-CM taxonomy. \n
     Based on the ICD-10-CM-XML export from
     """
-    raw_xml = ET.parse('resources\\ICD_10_CM_2022.xml')
+    raw_xml = ET.parse('resources/ICD_10_CM_2022.xml')
     root = raw_xml.getroot()
     tree = treelib.Tree()
     tree.create_node('ICD-10-CM', 0)
